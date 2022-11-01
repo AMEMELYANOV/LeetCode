43899fd4c75e2622f94b1defed8593c730e01805
AMEMELYANOV <emelyanov.am@gmail.com>
2022-11-01 21:54:31 +0300

    2022-11-01 часть 2

--- src/main/java/kot/Main.kt
+++ src/main/java/kot/Main.kt
@@ -5,4 +5,10 @@ fun main() {
     var name: String = "Ivan"
     println(name)
 
+    var str1: String? = "gdfgdf"
+    var str2: String? = null
+    var str3: String? = null
+
+    println((str1?.length ?: 0) + (str2?.length ?: 0) + (str3?.length ?: 0))
+
 }
